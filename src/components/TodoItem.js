30cecaf7685bccce1313b0517cc3a8c42d737f48
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-05 00:07:08 +0700

    component - Happy New Year

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div className='TodoItem'>
+        <p>Happy New Year</p>
+      </div>
+    )
+  }
+}
+
+export default TodoItem
