da427132eb721e951f419604052c7da7a59f1b96
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-08 13:52:36 +0700

    👑 todos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
@@ -7,49 +6,143 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todoItems: [
-        { title: 'Learn Go', isComplete: true },
-        { title: 'Learn React', isComplete: false },
-        { title: 'Learn Express', isComplete: true },
-        { title: 'Learn Mongo', isComplete: false },
-        { title: 'Learn Node', isComplete: false },
-        { title: 'Learn TypeScript', isComplete: true },
-        { title: 'Learn Python', status: false }
-      ]
+      text: '',
+      todoItems: [],
+      todosFilter: []
     }
   }
 
-  onItemClicked = (value) => {
-    const { todoItems } = this.state
-    const index = todoItems.indexOf(value)
+  componentWillMount() {
+    const data = JSON.parse(localStorage.getItem('todos')) || []
+    this.setState({ 
+      todoItems: data,
+      todosFilter: data
+    })
+  }
+
+  async handleItemClicked(value) {
+    const { todosFilter, todoItems } = this.state
+    const index = todosFilter.indexOf(value)
+    const _index = todoItems.indexOf(value)
     
-    this.setState({ todoItems: [
-      ...todoItems.slice(0, index),
-      {
-        ...value,
-        isComplete: !value.isComplete
-      },
-      ...todoItems.slice(index + 1)
-    ]})
+    await this.setState({
+      todosFilter: [
+        ...todosFilter.slice(0, index),
+        {
+          ...value,
+          isComplete: !value.isComplete
+        },
+        ...todosFilter.slice(index + 1)
+      ],
+      todoItems: [
+        ...todoItems.slice(0, _index),
+        {
+          ...value,
+          isComplete: !value.isComplete
+        },
+        ...todoItems.slice(_index + 1)
+      ]
+    })
+
+    localStorage.setItem('todos', JSON.stringify(this.state.todoItems))
+  }
+
+  handleValueChange(e) {
+    const { value } = e.target
+    this.setState({ text: value})
+  }
+
+  async handleKeyUp(e) {
+    if (e.keyCode !== 13) return
+
+    const { todoItems, text } = this.state
+    
+    await this.setState({
+      text: '',
+      todoItems: [
+        ...todoItems,
+        {
+          title: text,
+          isComplete: false
+        }
+      ],
+      todosFilter: [
+        ...todoItems,
+        {
+          title: text,
+          isComplete: false
+        }
+      ]
+    })
+
+    localStorage.setItem('todos', JSON.stringify(this.state.todoItems))
+  }
+
+  handleFilter(status) {
+    const { todoItems } = this.state
+    if (!todoItems) return
+
+    this.setState({
+      todosFilter: [
+        ...todoItems.filter(item => {
+          if (status === 0) return item
+          if (status === -1) return item.isComplete === false
+          return item.isComplete === true
+        })
+      ]
+    })
   }
 
   render() {
+    const { todosFilter } = this.state
+    const countTodos = todosFilter.length
+
     return (
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-          {
-            this.state.todoItems.length > 0 
-            && this.state.todoItems.map((item, index) =>
-              <TodoItem 
-                key={ index } 
-                item={ item } 
-                onClick={this.onItemClicked} 
-              />
-            )
-          }
-          {
-            this.state.todoItems.length === 0 && 'Nothing'
-          }
+        <h1 className="App-title">todos({countTodos})</h1>
+        <input 
+          className="input input-text" 
+          type="text" 
+          placeholder="Enter your todo..."
+          value={this.state.text}
+          onChange={this.handleValueChange.bind(this)}
+          onKeyUp={this.handleKeyUp.bind(this)}
+        />
+        <div className="group-button">
+          <button 
+            className="input input-button" 
+            type="button"
+            onClick={this.handleFilter.bind(this, 0)}
+          >
+            All
+          </button>
+          <button 
+            className="input input-button" 
+            type="button"
+            onClick={this.handleFilter.bind(this, -1)}
+          >
+            Active
+          </button>
+          <button 
+            className="input input-button" 
+            type="button"
+            onClick={this.handleFilter.bind(this, 1)}
+          >
+            Completed
+          </button>
+        </div>
+        {
+          todosFilter.length > 0 && todosFilter.map((item, index) =>
+            <TodoItem
+              key={ index } 
+              item={ item }
+              onClick={this.handleItemClicked.bind(this)} 
+            />
+          )
+        }
+        {
+          !todosFilter.length && <p className='empty'>Oh!!! Nothing...</p>
+        }
       </div>
     );
   }
